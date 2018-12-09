6ba972645e89cb121876aa79c1197cc29e8230aa
tehitz <santeri.siirila@gmail.com>
2018-12-09 12:41:04 +0200

    styles edited, alert added after removal

--- ListWhiskys.js
+++ ListWhiskys.js
@@ -15,15 +15,18 @@ if (!firebase.apps.length) {
 	firebaseApp = firebase.initializeApp(firebaseConfig);
 }
 
+
+
 export default class ListWhiskys extends React.Component {
 	static navigationOptions = {title: 'Listed whiskys'}
 	constructor(props) {
 		super(props);
 		this.itemsRef = firebaseApp.database().ref('whiskys');
 		this.state = {
-			name: '', desc: '', price: '', whiskys: [], modalVisible: false, rating: ''
+			name: '', desc: '', price: '', whiskys: [], modalVisible: false, rating: '', screenHeight: 0
 		};
 	}
+
 
 	keyExtractor = (item) => item.id;
 
@@ -31,17 +34,17 @@ export default class ListWhiskys extends React.Component {
 		return firebase.database().ref('whiskys').child(id).remove();
 	}
 
+
 	renderItem = ({item}) =>
-		<View>
-		<ScrollView style={{fontSize: 15, marginBottom: 20, justifyContent: 'center'}}>
+
+		<ScrollView
+			style={{fontSize: 15, paddingBottom: 10, marginHorizontal: 40}}>
 			<Text> Name: {item.name}</Text>
 			<Text> Description: {item.desc}</Text>
 			<Text> Price: {item.price}</Text>
 			<Text> Rating: {item.rating}/5</Text>
-			<Button onPress={() => this.handleRemove(item.id)} title="Delete"/>
-		</ScrollView>
-		<View styles={{marginTop: 100}}/>
-		</View>;
+			<Button onPress={() => this.handleRemove(item.id).then(alert('Whisky removed'))} title="Delete"/>
+		</ScrollView>;
 
 
 	listenForItems(itemsRef) {
@@ -68,20 +71,18 @@ export default class ListWhiskys extends React.Component {
 
 	render() {
 		return(
-			<View style= {{backgroundColor: 'white', alignItems: 'center'}}>
+			<View style= {{backgroundColor: 'white', paddingBottom: 10, flex: 1}}>
 				<Text style = {{marginTop: 10,
 					marginBottom: 10,
 					fontFamily: 'Helvetica',
 					fontSize: 36,
 					fontWeight: 'bold',
 					textAlign: 'center'}}>Whiskys added</Text>
-					<View styles={{marginBottom: 100}}>
-							<FlatList
+							<FlatList style={{width: window.width}}
 								data = {this.state.whiskys}
 								keyExtractor = {this.keyExtractor}
 								renderItem = {this.renderItem}
 							/>
-					</View>
 			</View>
 		);
 	}
